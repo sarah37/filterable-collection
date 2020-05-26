35cc7de275eec9bcecd5bdc2a108bb341e307942
Sarah Schöttler <sarah@schoettler.email>
2020-05-26 12:12:37 +0200

    prettier

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const taxonomy = {
 	node_representation: ["explicit", "aggregated", "abstract"],
 	link_representation: ["explicit", "aggregated", "abstract"],
 	composition: ["juxtaposed", "superimposed", "nested", "integrated"],
-	interactivity: ["none", "optional", "required", "interaction_technique"]
+	interactivity: ["none", "optional", "required", "interaction_technique"],
 };
 
 const facets = Object.keys(taxonomy);
@@ -19,16 +19,16 @@ var filters = d3
 	.data(facets)
 	.enter()
 	.append("div")
-	.attr("id", d => "select_" + d);
+	.attr("id", (d) => "select_" + d);
 
 filters
 	.append("h3")
 	// .html(d => '<div class="legend_circle ' + d + '"></div>' + formatText(d));
-	.html(d => formatText(d));
+	.html((d) => formatText(d));
 
 var checkboxes = filters
 	.selectAll("input")
-	.data(d => taxonomy[d])
+	.data((d) => taxonomy[d])
 	.enter()
 	.append("div")
 	.classed("checkbox-container", true);
@@ -36,19 +36,23 @@ checkboxes
 	.append("input")
 	.attr("type", "checkbox")
 	.attr("class", "input")
-	.attr("id", function(d) {
-		return "check_" + d3.select(this.parentNode.parentNode).datum() + "_" + d;
+	.attr("id", function (d) {
+		return (
+			"check_" + d3.select(this.parentNode.parentNode).datum() + "_" + d
+		);
 	})
-	.attr("value", d => d);
+	.attr("value", (d) => d);
 checkboxes
 	.append("label")
-	.attr("for", function(d) {
-		return "check_" + d3.select(this.parentNode.parentNode).datum() + "_" + d;
+	.attr("for", function (d) {
+		return (
+			"check_" + d3.select(this.parentNode.parentNode).datum() + "_" + d
+		);
 	})
 	.append("span")
-	.text(d => formatText(d));
+	.text((d) => formatText(d));
 
-d3.select("#showall").on("click", function() {
+d3.select("#showall").on("click", function () {
 	d3.selectAll("input").property("checked", false);
 	// dispatch event to reload techniques
 	let event = new Event("change");
@@ -56,19 +60,23 @@ d3.select("#showall").on("click", function() {
 });
 
 d3.csv(url)
-	.then(function(data) {
+	.then(function (data) {
 		console.log(data);
 
 		// display count
 		d3.selectAll("#count, #total").text(data.length);
 
 		// listen for changes in filters
-		d3.selectAll(".input").on("change", function() {
+		d3.selectAll(".input").on("change", function () {
 			// get filter values
-			var filters = facets.map(function(facet) {
+			var filters = facets.map(function (facet) {
 				var cats = [];
-				taxonomy[facet].forEach(function(cat) {
-					if (d3.select("#check_" + facet + "_" + cat).property("checked")) {
+				taxonomy[facet].forEach(function (cat) {
+					if (
+						d3
+							.select("#check_" + facet + "_" + cat)
+							.property("checked")
+					) {
 						cats.push(cat);
 					}
 				});
@@ -81,13 +89,13 @@ d3.csv(url)
 
 		function refreshTechniques(filters) {
 			// filter
-			var fData = data.filter(d => filterData(d, filters));
+			var fData = data.filter((d) => filterData(d, filters));
 			// update count in heading
 			d3.select("#count").text(fData.length);
 			// get IDs of techniques matching filter
-			var ids = fData.map(d => d.image);
+			var ids = fData.map((d) => d.image);
 			// hide all non-matching ones
-			d3.selectAll(".grid-item").style("display", d =>
+			d3.selectAll(".grid-item").style("display", (d) =>
 				ids.indexOf(d.image) != -1 ? null : "none"
 			);
 			// update layout
@@ -102,54 +110,51 @@ d3.csv(url)
 			.append("div")
 			.classed("grid-item", true);
 
-		div.append("img").attr("src", d => "img/" + d.image + ".png");
-		div.append("h2").text(d => d.Title);
-		div
-			.append("span")
-			.html(d =>
-				[
-					d.Author,
-					". <i>",
-					d["Publication Title"],
-					"</i> (",
-					d["Publication Year"],
-					")",
-					d.DOI == ""
-						? ""
-						: " <a href=https://doi.org/" +
-						  d.DOI +
-						  ' target="_blank">[DOI Link]</a>',
-					"<br>"
-				].join("")
-			);
+		div.append("img").attr("src", (d) => "img/" + d.image + ".png");
+		div.append("h2").text((d) => d.Title);
+		div.append("span").html((d) =>
+			[
+				d.Author,
+				". <i>",
+				d["Publication Title"],
+				"</i> (",
+				d["Publication Year"],
+				")",
+				d.DOI == ""
+					? ""
+					: " <a href=https://doi.org/" +
+					  d.DOI +
+					  ' target="_blank">[DOI Link]</a>',
+				"<br>",
+			].join("")
+		);
 		var tags = div.append("div").style("margin-top", "7px");
 
 		// add tags on technique cards
-		facets.forEach(function(facet) {
-			tags
-				.append("div")
+		facets.forEach(function (facet) {
+			tags.append("div")
 				.classed("tag", true)
 				.classed(facet, true)
-				.html(d => d[facet]);
+				.html((d) => d[facet]);
 		});
 	})
-	.then(function() {
-		imagesLoaded(".grid", function() {
+	.then(function () {
+		imagesLoaded(".grid", function () {
 			var elem = document.querySelector(".grid");
 			window.msnry = new Masonry(elem, {
 				// options
 				itemSelector: ".grid-item",
 				columnWidth: 241,
-				gutter: 15
+				gutter: 15,
 			});
 		});
 	})
-	.catch(function(error) {
+	.catch(function (error) {
 		throw error;
 	});
 
 function filterData(d, filters) {
-	return filters.every(function(fil) {
+	return filters.every(function (fil) {
 		// facet: fil[0]
 		// selected: fil[1]
 		// check if either array is empty or category is selected
@@ -158,7 +163,7 @@ function filterData(d, filters) {
 }
 
 function unique(arr, acc) {
-	return arr.map(acc).filter(function(value, index, self) {
+	return arr.map(acc).filter(function (value, index, self) {
 		return self.indexOf(value) === index;
 	});
 }
@@ -172,7 +177,9 @@ function formatText(str) {
 		str =
 			str.slice(0, str.indexOf("_")) +
 			" " +
-			str.slice(str.indexOf("_") + 1, str.indexOf("_") + 2).toUpperCase() +
+			str
+				.slice(str.indexOf("_") + 1, str.indexOf("_") + 2)
+				.toUpperCase() +
 			str.slice(str.indexOf("_") + 2);
 	}
 	return str;
