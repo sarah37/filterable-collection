83cdac44c0577d1d2e8a374bf8534c4a2a0413b8
Sarah Schöttler <sarah@schoettler.email>
2021-11-14 18:38:15 +0000

    create template version

--- script.js
+++ script.js
@@ -1,98 +1,80 @@
-const url = "geonetworks.csv";
-
-const taxonomy = {
-	geography_representation: ["mapped", "distorted", "abstract"],
-	node_representation: ["explicit", "aggregated", "abstract"],
-	link_representation: ["explicit", "aggregated", "abstract"],
-	composition: ["juxtaposed", "superimposed", "nested", "integrated"],
-	interactivity: ["not_required", "required", "interaction_only"],
-};
-
-const facets = Object.keys(taxonomy);
-
-const datatypes = [
-	// 1/ Links
-	"directed_links",
-	"undirected_links",
-	"weighted_links",
-	"additional_link_attributes",
-	// "no_additional_link_attributes",
-	"additional_node_attributes",
-	// 2/ Geolocations
-	"exact_point_locations",
-	"area_locations",
-	"colocated_nodes",
-	// 3/ Density
-	"dense_networks",
-	"networks_with_varying_density",
-	// 4/ Dynamic
-	"dynamic_networks",
-	// 5/ Uncertainty
-	"uncertain_network_topology",
-	"uncertain_locations",
-	"uncertain_additional_attributes",
-];
+// You should not need to modify anything in this file to set up your own survey website
 
 const container = d3.select(".grid");
 
+const facets = !taxonomy || Object.keys(taxonomy);
+
 // create checkboxes to filter techniques
-var filters = d3
-	.select("#filters")
-	.selectAll("div")
-	.data(facets)
-	.enter()
-	.append("div")
-	.attr("id", (d) => "select_" + d);
-
-filters
-	.append("h3")
-	// .html(d => '<div class="legend_circle ' + d + '"></div>' + formatText(d));
-	.html((d) => formatText(d));
-
-var checkboxes = filters
-	.selectAll("input")
-	.data((d) => taxonomy[d])
-	.enter()
-	.append("div")
-	.classed("checkbox-container", true);
-checkboxes
-	.append("input")
-	.attr("type", "checkbox")
-	.attr("class", "input")
-	.attr("id", function (d) {
-		return (
-			"check_" + d3.select(this.parentNode.parentNode).datum() + "_" + d
+if (taxonomy != null) {
+	var filters = d3
+		.select("#filters")
+		.selectAll("div")
+		.data(facets)
+		.enter()
+		.append("div")
+		.attr("id", (d) => "select_" + d)
+		.attr(
+			"class",
+			(d) =>
+				"select_width" +
+				((taxonomy[d].length == 2) | (taxonomy[d].length == 4) ? 2 : 3)
 		);
-	})
-	.attr("value", (d) => d);
-checkboxes
-	.append("label")
-	.attr("for", function (d) {
-		return (
-			"check_" + d3.select(this.parentNode.parentNode).datum() + "_" + d
-		);
-	})
-	.append("span")
-	.text((d) => formatText(d));
 
-// checkboxes for data types
-var checkData = d3
-	.select("#filters_data")
-	.selectAll("div")
-	.data(datatypes)
-	.enter()
-	.append("div");
-checkData
-	.append("input")
-	.attr("type", "checkbox")
-	.attr("class", "input")
-	.attr("id", (d) => "check_" + d)
-	.attr("value", (d) => d);
-checkData
-	.append("label")
-	.attr("for", (d) => "check_" + d)
-	.append("span")
-	.text((d) => sentenceCase(d));
+	filters.append("h3").html((d) => formatText(d));
+
+	var checkboxes = filters
+		.selectAll("input")
+		.data((d) => taxonomy[d])
+		.enter()
+		.append("div")
+		.classed("checkbox-container", true);
+	checkboxes
+		.append("input")
+		.attr("type", "checkbox")
+		.attr("class", "input")
+		.attr("id", function (d) {
+			return (
+				"check_" +
+				d3.select(this.parentNode.parentNode).datum() +
+				"_" +
+				d
+			);
+		})
+		.attr("value", (d) => d);
+	checkboxes
+		.append("label")
+		.attr("for", function (d) {
+			return (
+				"check_" +
+				d3.select(this.parentNode.parentNode).datum() +
+				"_" +
+				d
+			);
+		})
+		.append("span")
+		.text((d) => formatText(d));
+}
+
+if (tags) {
+	// checkboxes for data types
+	var checkData = d3
+		.select("#filters_data")
+		.selectAll("div")
+		.data(tags)
+		.enter()
+		.append("div");
+	checkData
+		.append("input")
+		.attr("type", "checkbox")
+		.attr("class", "input")
+		.attr("id", (d) => "check_" + d)
+		.attr("value", (d) => d);
+	checkData
+		.append("label")
+		.attr("for", (d) => "check_" + d)
+		.append("span")
+		.text((d) => sentenceCase(d));
+}
 
 d3.select("#showall").on("click", function () {
 	d3.selectAll("input").property("checked", false);
@@ -101,7 +83,10 @@ d3.select("#showall").on("click", function () {
 	eventHandler.dispatchEvent(event);
 });
 
-d3.csv(url)
+d3.csv(url, (d, i) => {
+	d.id = i;
+	return d;
+})
 	.then(function (data) {
 		console.log(data);
 
@@ -111,23 +96,30 @@ d3.csv(url)
 		// listen for changes in filters
 		d3.selectAll(".input").on("change", function () {
 			// get filter values
-			var filters = facets.map(function (facet) {
-				var cats = [];
-				taxonomy[facet].forEach(function (cat) {
-					if (
-						d3
-							.select("#check_" + facet + "_" + cat)
-							.property("checked")
-					) {
-						cats.push(cat);
-					}
+			if (taxonomy != null) {
+				var filters = facets.map(function (facet) {
+					var cats = [];
+					taxonomy[facet].forEach(function (cat) {
+						if (
+							d3
+								.select("#check_" + facet + "_" + cat)
+								.property("checked")
+						) {
+							cats.push(cat);
+						}
+					});
+					return [facet, cats];
 				});
-				return [facet, cats];
-			});
-			var dataFilters = datatypes.filter(function (d) {
-				return d3.select("#check_" + d).property("checked");
-			});
-
+			} else {
+				var filters = null;
+			}
+			if (tags != null) {
+				var dataFilters = tags.filter(function (d) {
+					return d3.select("#check_" + d).property("checked");
+				});
+			} else {
+				var dataFilters = null;
+			}
 			// update
 			refreshTechniques(filters, dataFilters);
 		});
@@ -138,10 +130,10 @@ d3.csv(url)
 			// update count in heading
 			d3.select("#count").text(fData.length);
 			// get IDs of techniques matching filter
-			var ids = fData.map((d) => d.image);
+			var ids = fData.map((d) => d.id);
 			// hide all non-matching ones
 			d3.selectAll(".grid-item").style("display", (d) =>
-				ids.indexOf(d.image) != -1 ? null : "none"
+				ids.indexOf(d.id) != -1 ? null : "none"
 			);
 			// update layout
 			msnry.layout();
@@ -155,29 +147,27 @@ d3.csv(url)
 			.append("div")
 			.classed("grid-item", true);
 
-		div.append("img").attr("src", (d) => "img/" + d.image + ".png");
-		div.append("h2").text((d) => d.Title);
-		div.append("span").html((d) =>
-			[
-				d.Author,
-				". <i>",
-				d["Publication Title"],
-				"</i> (",
-				d["Publication Year"],
-				")",
-				" <a href=" + d.URL + ' target="_blank">[Link]</a>',
-				"<br>",
-			].join("")
-		);
-		var tags = div.append("div").style("margin-top", "7px");
+		// show image if defined
+		div.filter((d) => d.image != "")
+			.append("img")
+			.attr("src", (d) => "img/" + d.image);
 
-		// add tags on technique cards
-		facets.forEach(function (facet) {
-			tags.append("div")
-				.classed("tag", true)
-				.classed(facet, true)
-				.html((d) => d[facet]);
-		});
+		// add div for text content (modifiable in config)
+		div.append("div").call(cardContent);
+
+		// add taxonomy tags
+		var tax_tags = div.append("div").style("margin-top", "7px");
+
+		// add tags on technique cards (if taxonomy is defined)
+		if (taxonomy) {
+			facets.forEach(function (facet) {
+				tax_tags
+					.append("div")
+					.classed("tag", true)
+					.classed(facet, true)
+					.html((d) => d[facet]);
+			});
+		}
 	})
 	.then(function () {
 		imagesLoaded(".grid", function () {
@@ -196,15 +186,17 @@ d3.csv(url)
 
 function filterData(d, filters, dataFilters) {
 	return (
-		filters.every(function (fil) {
-			// facet: fil[0]
-			// selected: fil[1]
-			// check if either array is empty or category is selected
-			return fil[1].length == 0 || fil[1].indexOf(d[fil[0]]) != -1;
-		}) &&
-		dataFilters.every(function (fil) {
-			return d[fil] == "yes";
-		})
+		(filters == null ||
+			filters.every(function (fil) {
+				// facet: fil[0]
+				// selected: fil[1]
+				// check if either array is empty or category is selected
+				return fil[1].length == 0 || fil[1].indexOf(d[fil[0]]) != -1;
+			})) &&
+		(dataFilters == null ||
+			dataFilters.every(function (fil) {
+				return d[fil] == "yes";
+			}))
 	);
 }
 
